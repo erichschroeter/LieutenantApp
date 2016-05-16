4e37fd7676d65ea77f16d1d12ac855a5b3a57a84
Erich Schroeter <erichschroeter@gmail.com>
2016-05-16 07:26:30 -0500

    Added a click listener to FAB to start the EntryActivity

--- app/src/main/java/com/lieutenant/erichschroeter/lieutenantapp/MainActivity.java
+++ app/src/main/java/com/lieutenant/erichschroeter/lieutenantapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.lieutenant.erichschroeter.lieutenantapp;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent newEntryIntent = new Intent(MainActivity.this, EntryActivity.class);
+                MainActivity.this.startActivity(newEntryIntent);
+            }
+        });
     }
 }
