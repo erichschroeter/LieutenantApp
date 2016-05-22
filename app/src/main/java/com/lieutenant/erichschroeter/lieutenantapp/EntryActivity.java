29b156f06eacfcab22706c9de847201fb930b8cc
Erich Schroeter <erich.schroeter@magnetek.com>
2016-05-22 18:11:41 -0500

    Started implementing EntryActivity

--- app/src/main/java/com/lieutenant/erichschroeter/lieutenantapp/EntryActivity.java
+++ app/src/main/java/com/lieutenant/erichschroeter/lieutenantapp/EntryActivity.java
@@ -1,13 +1,23 @@
 package com.lieutenant.erichschroeter.lieutenantapp;
 
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class EntryActivity extends AppCompatActivity {
+
+    private LieutenantDbHelper _db;
+
+    TextView _entryText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +25,37 @@ public class EntryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_entry);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        _db = new LieutenantDbHelper(getApplicationContext());
+
+        Intent intent = getIntent();
+        if (intent != null)
+        {
+            intent.getIntExtra(LieutenantContract.Entry._ID, 0);
+            SQLiteDatabase db = _db.getReadableDatabase();
+            db.g
+        }
+
+        _entryText = (TextView) findViewById(R.id.editEntryText);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_entry, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_save:
+                // Save the entry data.
+                Toast.makeText(EntryActivity.this, "Saving...", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                // If we got here, the user's action was not recognized.
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
