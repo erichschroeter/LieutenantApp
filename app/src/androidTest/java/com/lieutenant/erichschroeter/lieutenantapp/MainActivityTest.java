540f1877754a97bd618c1933aa4eaa734e065d71
Erich Schroeter <erich.schroeter@gmail.com>
2016-05-24 20:42:22 -0500

    Added initial testCreateNewEntry unit test to assert that the EntryActivity is started

--- app/src/androidTest/java/com/lieutenant/erichschroeter/lieutenantapp/MainActivityTest.java
+++ app/src/androidTest/java/com/lieutenant/erichschroeter/lieutenantapp/MainActivityTest.java
@@ -1,13 +1,19 @@
 package com.lieutenant.erichschroeter.lieutenantapp;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.support.design.widget.FloatingActionButton;
 import android.test.ActivityInstrumentationTestCase2;
+import android.test.TouchUtils;
 import android.test.suitebuilder.annotation.SmallTest;
 
 /**
  * Created by erich on 5/24/16.
  */
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    private Activity activity;
+    private FloatingActionButton fab;
 
     public MainActivityTest() {
         super(MainActivity.class);
@@ -16,11 +22,21 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
     @Override
     protected void setUp() throws Exception {
         super.setUp();
+        activity = getActivity();
+        fab = (FloatingActionButton) activity.findViewById(R.id.fab);
     }
 
     @SmallTest
     public void testAddEntryButtonExists() {
-        FloatingActionButton fab = (FloatingActionButton) getActivity().findViewById(R.id.fab);
         assertNotNull(fab);
+    }
+
+    public void testCreateNewEntry() {
+        Instrumentation.ActivityMonitor activityMonitor = getInstrumentation().addMonitor(EntryActivity.class.getName(), null, false);
+        TouchUtils.tapView(this, fab);
+        // assertThat EntryActivity is started
+        EntryActivity entryActivity = (EntryActivity)getInstrumentation().waitForMonitorWithTimeout(activityMonitor, 5000);
+        assertNotNull(entryActivity);
+        entryActivity.finish();
     }
 }
